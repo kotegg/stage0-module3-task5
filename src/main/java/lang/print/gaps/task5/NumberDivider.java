2a0491c27443dee2c174ad1b998215fcc592d3c2
kotegg <lolpol.kek123@gmail.com>
2023-07-06 20:37:18 +0300

    1

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,7 +1,7 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+    void divide(double toBeDivided) {
         System.out.print(toBeDivided/5);
     }
 }
