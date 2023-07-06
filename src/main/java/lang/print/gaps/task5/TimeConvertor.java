8c17277e53945819a5a6241b8e9768dda32e1043
kotegg <lolpol.kek123@gmail.com>
2023-07-06 20:32:53 +0300

    1

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,6 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        System.out.print(minutes + " минут это " + minutes * 60 + "секунд");
     }
 }
